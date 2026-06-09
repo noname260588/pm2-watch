4a236cd3b39a0f639f2593c38a2e729d460045ce
noname260588 <hongphuong260588@gmail.com>
2026-06-09 13:31:31 +0700

    Fix PM2 serve 403 Forbidden bug on Windows by forcing forward slashes in path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,7 +28,7 @@ module.exports = {
       name: "pm2-watch-ui",
       script: "serve",
       env: {
-        PM2_SERVE_PATH: path.join(__dirname, "frontend/dist"),
+        PM2_SERVE_PATH: path.join(__dirname, "frontend/dist").replace(/\\/g, '/'),
         PM2_SERVE_PORT: 5173,
         PM2_SERVE_SPA: 'true',
         PM2_SERVE_HOMEPAGE: '/index.html'
